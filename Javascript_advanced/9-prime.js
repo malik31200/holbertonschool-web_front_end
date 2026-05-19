37fea341434b2710e9eb2c6801fe905f0409d549
malik31200 <yka-vtc@outlook.fr>
2026-05-19 09:17:53 +0200

    add function of Prime numbers & timing execution

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,36 @@
+function countPrimeNumbers(){
+    let count = 0;
+
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+
+        // Check if i is divible be an other number
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        // If the number is prime
+        if (isPrime) {
+            count++;
+        }
+    }
+
+    return count
+}
+
+// Start chrono
+const start = performance.now();
+
+// Run of the function
+countPrimeNumbers();
+
+// End chrono
+const end = performance.now();
+
+// total time
+const timeUsed = end -start;
+
+console.log ("Execution time of printing countPrimeNumbers was " + timeUsed + " milliseconds.");
